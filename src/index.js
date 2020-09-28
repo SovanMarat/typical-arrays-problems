a47c301c8951122dceb01798d0391f795eb075b5
SovanMarat <maratwp@mail.ru>
2020-09-28 16:26:54 +0300

    init: task finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length > 0 || array === []) {
+        let resultMin = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < resultMin) {
+                resultMin = array[i];
+            }
+        }
+        return resultMin;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length > 0 || array === []) {
+        let resultMin = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > resultMin) {
+                resultMin = array[i];
+            }
+        }
+        return resultMin;
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length > 0 || array === []) {
+        let resultSum = 0;
+        for (let i = 0; i < array.length; i++) {
+            resultSum += array[i];
+        }
+        let resultAvg = resultSum / array.length;
+        return resultAvg;
+    }
+    return 0;
 }
